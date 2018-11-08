1248450e02fd69382400b496f86e48af31cb9b63
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2018-11-07 23:41:21 -0800

    props added to nav

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Nav.css';
 import { Navbar, Button } from "react-bootstrap";
 
-class Nav extends Component {
-    render() {
-        return (
-            <Navbar>
-                <Navbar.Header>
-                    <Navbar.Brand >
-                        <a href="#home">Cooldowns</a>
-                    </Navbar.Brand>
-                    <Button bsStyle="primary">Primary</Button>
-                    <Navbar.Toggle />
-                </Navbar.Header>
-                <Navbar.Collapse >
-                    <Navbar.Text pullRight>
-                        Signed in as: <Navbar.Link href="#">User Name Goes here</Navbar.Link>
-                    </Navbar.Text>
-                    <Navbar.Text pullRight>Display something for user</Navbar.Text>
-                </Navbar.Collapse>
-            </Navbar>
-        )
-    }
-}
+const Nav = (props) => (
+    <Navbar>
+        <Navbar.Header>
+            <Navbar.Brand >
+                <a href="#home">Cooldowns</a>
+            </Navbar.Brand>
+            <Button bsStyle="primary">Primary</Button>
+            <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse >
+            <Navbar.Text pullRight>
+                Signed in as: {props.user ? 
+                <img src={props.user.photoURL} alt="Your Profile" />
+                :
+                <Navbar.Text>POOP YOU AIN'T</Navbar.Text>}
+            </Navbar.Text>
+            <Navbar.Text pullRight>Display something for user</Navbar.Text>
+        </Navbar.Collapse>
+    </Navbar>
+)
 
 export default Nav
