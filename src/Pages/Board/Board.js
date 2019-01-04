0bed8a969b506bb9102d2ee949b7c17440714738
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2019-01-03 23:50:15 -0800

    mock hunting request

--- src/Pages/Board/Board.js
+++ src/Pages/Board/Board.js
@@ -1,14 +1,51 @@
 import React from 'react';
 import "./Board.css";
-import { Grid, Row, Col } from "react-bootstrap";
+import { Grid, Row, Col, Panel, Button } from "react-bootstrap";
 
 class Board extends React.Component {
     render() {
         return (
             <Grid>
-                <Row className="show-grid" bsClass="text-center">
-                    <Col xs={12} center>
-                        <p>poop centered</p>
+                <Row className="show-grid">
+                    <Col xs={10} center>
+                        <Panel bsClass="bg-warning">
+                            <Row>
+                                <Col xs={9}>
+                                    <p className="text-left">Hunting Request</p>
+                                </Col>
+                                <Col xs={3}>
+                                    <p className="text-right">Posted: January 3, 2019 14:00</p>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={6} className="text-left">
+                                    <p>Poster: Mosjoandy</p>
+                                    <p>Fulfilled: Imp Farmer</p>
+                                    <br />
+                                    <p>Payout: 3.5m</p>
+                                </Col>
+                                <Col xs={6} className="text-right">
+                                    <p>Item: Crystal Fragment</p>
+                                    <p>Quantity: 690</p>
+                                    <br />
+                                    <p>Status: In Progress</p>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={3} className="text-center">
+                                    <Button>Accept</Button>
+                                </Col>
+                                <Col xs={3} className="text-center">
+                                    <Button>Items Sent</Button>
+                                </Col>
+                                <Col xs={3} className="text-center">
+                                    <Button>Payment Sent</Button>
+                                </Col>
+                                <Col xs={3} className="text-center">
+                                    <p>something</p>
+                                </Col>
+                            </Row>
+                        </Panel>
                     </Col>
                 </Row>
             </Grid>
