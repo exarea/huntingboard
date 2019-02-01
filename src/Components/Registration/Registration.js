8a9e226916457aa249c52be923cf1369cc7024fb
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2019-02-01 15:47:39 -0800

    loading gif in registration

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -9,6 +9,7 @@ class Registration extends React.Component {
         this.state = {
             show: false,
             registrationSuccess: false,
+            spinnerShow: false,
 
             email: "",
             ign: "",
@@ -20,6 +21,8 @@ class Registration extends React.Component {
 
         this.handleShow = this.handleShow.bind(this);
         this.handleClose = this.handleClose.bind(this);
+
+        this.spinnyThing = this.spinnyThing.bind(this);
     };
 
     handleChange = (event) => {
@@ -28,6 +31,9 @@ class Registration extends React.Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
+        this.setState({
+            spinnerShow: true
+        });
         const { email, password } = this.state;
         auth.createUserWithEmailAndPassword(email, password)
             .then((result) => {
@@ -61,6 +67,12 @@ class Registration extends React.Component {
         });
     };
 
+    spinnyThing() {
+        this.setState({
+            spinnerShow: true
+        })
+    }
+
     render() {
         const { email, password, ign, error } = this.state;
         return (
@@ -68,124 +80,132 @@ class Registration extends React.Component {
             <div>
                 <Button style={{ marginRight: 5 }} onClick={this.handleShow}>New Account</Button>
                 <Modal show={this.state.show} onHide={this.handleClose}>
-                    {this.state.registrationSuccess === false ?
-                        <Modal.Body>
-                            <h1>Registration</h1>
-                            {error ?
-                                (<div>{error.message}</div>)
-                                :
-                                null}
-                            <Form onSubmit={this.handleSubmit}>
-                                <FormGroup>
-                                    <InputGroup>
-                                        <Label>E-mail:</Label>
-                                        <FormControl
-                                            type="text"
-                                            name="email"
-                                            className="form-control"
-                                            placeholder="Email"
-                                            value={email}
-                                            onChange={this.handleChange}
-                                            required
-                                        />
-                                    </InputGroup>
-                                    <br />
-                                    <InputGroup>
-                                        <Label>Password:</Label>
-                                        <FormControl
-                                            type="password"
-                                            name="password"
-                                            className="form-control"
-                                            placeholder="Password"
-                                            value={password}
-                                            onChange={this.handleChange}
-                                            required
-                                        />
-                                    </InputGroup>
-                                    <br />
-                                    <InputGroup>
-                                        <Label>In-Game Name:</Label>
-                                        <FormControl
-                                            type="text"
-                                            name="ign"
-                                            className="form-control"
-                                            placeholder="Mosjoandy"
-                                            maxLength="24"
-                                            value={ign}
-                                            onChange={this.handleChange}
-                                            required
-                                        />
-                                    </InputGroup>
-                                    <br />
+                    {
+                        this.state.registrationSuccess === false ?
+                            <Modal.Body>
+                                <h1>Registration</h1>
+                                <Form onSubmit={this.handleSubmit}>
+                                    <FormGroup>
+                                        <InputGroup>
+                                            <Label>E-mail:</Label>
+                                            <FormControl
+                                                type="text"
+                                                name="email"
+                                                className="form-control"
+                                                placeholder="Email"
+                                                value={email}
+                                                onChange={this.handleChange}
+                                                required
+                                            />
+                                        </InputGroup>
+                                        <br />
+                                        <InputGroup>
+                                            <Label>Password:</Label>
+                                            <FormControl
+                                                type="password"
+                                                name="password"
+                                                className="form-control"
+                                                placeholder="Password"
+                                                value={password}
+                                                onChange={this.handleChange}
+                                                required
+                                            />
+                                        </InputGroup>
+                                        <br />
+                                        <InputGroup>
+                                            <Label>In-Game Name:</Label>
+                                            <FormControl
+                                                type="text"
+                                                name="ign"
+                                                className="form-control"
+                                                placeholder="Mosjoandy"
+                                                maxLength="24"
+                                                value={ign}
+                                                onChange={this.handleChange}
+                                                required
+                                            />
+                                        </InputGroup>
+                                        <br />
+                                        {error ?
+                                            (<div>{error.message}</div>)
+                                            :
+                                            null
+                                        }
+                                        
+                                        {
+                                            this.state.spinnerShow === true && error === null ?
+                                                <img style={{ width: 90, height: 75 }} src={require("../../data/loading.gif")} alt="spins" />
+                                                :
+                                                <Button
+                                                    type="submit"
+                                                    value="Submit">
+                                                    Register
+                                                </Button>
+                                        }
 
-                                    <Button
-                                        type="submit"
-                                        value="Submit">
-                                        Register
-                                    </Button>
-                                </FormGroup>
-                            </Form>
-                        </Modal.Body>
-                        :
-                        <Modal.Body>
-                            <h1>Registration</h1>
-                            {error ?
-                                (<div>{error.message}</div>)
-                                :
-                                null}
-                            <Form onSubmit={this.handleSubmit}>
-                                <FormGroup>
-                                    <InputGroup>
-                                        <Label>E-mail:</Label>
-                                        <FormControl
-                                            type="text"
-                                            name="email"
-                                            className="form-control"
-                                            placeholder={this.state.email}
-                                            value={email}
-                                            onChange={this.handleChange}
-                                            disabled
-                                        />
-                                    </InputGroup>
-                                    <br />
-                                    <InputGroup>
-                                        <Label>Password:</Label>
-                                        <FormControl
-                                            type="password"
-                                            name="password"
-                                            className="form-control"
-                                            placeholder="******"
-                                            value={password}
-                                            onChange={this.handleChange}
-                                            disabled
-                                        />
-                                    </InputGroup>
-                                    <br />
-                                    <InputGroup>
-                                        <Label>In-Game Name:</Label>
-                                        <FormControl
-                                            type="text"
-                                            name="ign"
-                                            className="form-control"
-                                            placeholder={this.state.ign}
-                                            maxLength="24"
-                                            value={ign}
-                                            onChange={this.handleChange}
-                                            disabled
-                                        />
-                                    </InputGroup>
-                                    <br />
+                                    </FormGroup>
+                                </Form>
+                            </Modal.Body>
+                            :
+                            <Modal.Body>
+                                <h1>Registration</h1>
+                                {error ?
+                                    (<div>{error.message}</div>)
+                                    :
+                                    null}
+                                <Form onSubmit={this.handleSubmit}>
+                                    <FormGroup>
+                                        <InputGroup>
+                                            <Label>E-mail:</Label>
+                                            <FormControl
+                                                type="text"
+                                                name="email"
+                                                className="form-control"
+                                                placeholder={this.state.email}
+                                                value={email}
+                                                onChange={this.handleChange}
+                                                disabled
+                                            />
+                                        </InputGroup>
+                                        <br />
+                                        <InputGroup>
+                                            <Label>Password:</Label>
+                                            <FormControl
+                                                type="password"
+                                                name="password"
+                                                className="form-control"
+                                                placeholder="******"
+                                                value={password}
+                                                onChange={this.handleChange}
+                                                disabled
+                                            />
+                                        </InputGroup>
+                                        <br />
+                                        <InputGroup>
+                                            <Label>In-Game Name:</Label>
+                                            <FormControl
+                                                type="text"
+                                                name="ign"
+                                                className="form-control"
+                                                placeholder={this.state.ign}
+                                                maxLength="24"
+                                                value={ign}
+                                                onChange={this.handleChange}
+                                                disabled
+                                            />
+                                        </InputGroup>
+                                        <br />
 
-                                    <Button
-                                        type="submit"
-                                        value="Submit"
-                                        disabled>
-                                        Register
+                                        <Button
+                                            type="submit"
+                                            value="Submit"
+                                            disabled>
+                                            Register
                                     </Button>
-                                    <p>Registration Successful!</p>
-                                </FormGroup>
-                            </Form>
-                        </Modal.Body>
+                                        <p>Registration Successful!</p>
+                                    </FormGroup>
+                                </Form>
+                            </Modal.Body>
                     }
                     <Modal.Footer>
                         <Button onClick={this.handleClose}>Close</Button>
