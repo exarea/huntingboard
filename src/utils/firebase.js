98f561ae8f003abee99c419ca9c9e11ae5f8b2aa
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2019-02-05 00:25:06 -0800

    favicon update

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCpuQ5pdfU167fCJN2PNaL-EUM4nfSPX20",
-    authDomain: "cooldown-8643b.firebaseapp.com",
-    databaseURL: "https://cooldown-8643b.firebaseio.com",
-    projectId: "cooldown-8643b",
-    storageBucket: "",
-    messagingSenderId: "443166393881"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 firebase.initializeApp(config);
 console.log("start your engines!");
